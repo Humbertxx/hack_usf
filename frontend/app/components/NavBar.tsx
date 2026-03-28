c7f600a54847e93421314fae560edf7376e7526e
cala28124-sketch <cala28124@gmail.com>
2026-03-28 16:24:58 -0400

    timelinedone

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -6,15 +6,24 @@ export default function NavBar() {
       <nav className="w-[100%] py-4 bg-white shadow-lg flex items-center justify-center max-md:gap-5 md:justify-around">
         <div className="font-bold text-2xl">Hows Grandma?</div>
         <div className="flex justify-around items-center justify-center gap-10">
-          <Link className="bg-green-100 p-2 rounded" href="/">
+          <Link
+            className="bg-green-100 p-2 rounded hover:scale-110 transition duration-100 ease-in"
+            href="/"
+          >
             Home
           </Link>
 
-          <Link className="bg-green-100 p-2 rounded" href="/timeline">
+          <Link
+            className="bg-green-100 p-2 rounded hover:scale-110 transition duration-100 ease-in"
+            href="/timeline"
+          >
             Timeline
           </Link>
 
-          <Link className="bg-green-100 p-2 rounded" href="/insights">
+          <Link
+            className="bg-green-100 p-2 rounded hover:scale-110 transition duration-100 ease-in"
+            href="/insights"
+          >
             Insights
           </Link>
         </div>
