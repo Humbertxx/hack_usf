e5ae0f8ea2de3cac6873b704b9b206e077bb2c60
cala28124-sketch <cala28124@gmail.com>
2026-03-28 13:44:37 -0400

    navbar

---
+++ frontend/app/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="w-[100%] py-4 bg-white shadow-lg flex items-center justify-around">
+        <div className="font-bold text-2xl">Hows Grandma?</div>
+        <div className="flex justify-around items-center justify-center gap-10">
+          <Link className="bg-green-100 p-2 rounded" href="/">
+            Home
+          </Link>
+
+          <Link className="bg-green-100 p-2 rounded" href="/timeline">
+            Timeline
+          </Link>
+
+          <Link className="bg-green-100 p-2 rounded" href="/insights">
+            Insights
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
