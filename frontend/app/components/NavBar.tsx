f7f7ba7d266b71b11932cb6a530a2928f73b0572
cala28124-sketch <cala28124@gmail.com>
2026-03-28 15:30:55 -0400

    basicfrontpage

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <>
-      <nav className="w-[100%] py-4 bg-white shadow-lg flex items-center justify-around">
+      <nav className="w-[100%] py-4 bg-white shadow-lg flex items-center justify-center max-md:gap-5 md:justify-around">
         <div className="font-bold text-2xl">Hows Grandma?</div>
         <div className="flex justify-around items-center justify-center gap-10">
           <Link className="bg-green-100 p-2 rounded" href="/">
